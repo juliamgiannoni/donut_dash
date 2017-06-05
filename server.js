e4e5520dd0afd1ff911a79bc98f15698e4f90caf
Julia Yi <juliamijiyi@gmail.com>
2017-06-05 16:44:56 -0700

    Set up server.js

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
+require('dotenv').config();
+require('./config/database');
+
 var app = express();
 
 app.use(logger('dev'));
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(bodyParser.json());
+app.use(require('./config/auth'));
+
+app.use('/api/customers', require('./routes/api/customers'));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
