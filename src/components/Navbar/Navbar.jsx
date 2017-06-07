c54d0d3521ecc36206f19ffb83ec9281d58cadce
Julia Yi <juliamijiyi@gmail.com>
2017-06-07 11:10:48 -0700

    Style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,12 +5,12 @@ import './Navbar.css';
 const Navbar = (props) => {
   let nav = props.customer ?
     <ul className='nav navbar-nav navbar-right'>
-      <li><Link to=''>{props.customer.name}'s Deliveries</Link></li>
-      <li><Link to='' onClick={props.handleLogout}>Log Out</Link></li>
+      <li className='Navbar-Link'><Link to=''>{props.customer.name}'s Deliveries</Link></li>
+      <li className='Navbar-Link'><Link to='' onClick={props.handleLogout}>Log Out</Link></li>
     </ul> :
     <ul className='nav navbar-nav navbar-right'>
-      <li><Link to='/login'>Log In</Link></li>
-      <li><Link to='/signup'>Sign Up</Link></li>
+      <li className='Navbar-Link'><Link to='/login'>Log In</Link></li>
+      <li className='Navbar-Link'><Link to='/signup'>Sign Up</Link></li>
     </ul>;
 
   return (
