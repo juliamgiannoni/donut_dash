23089b88c5b1b8453e1a75ea0373957520fd5997
Julia Yi <juliamijiyi@gmail.com>
2017-06-08 16:04:33 -0700

    Update cart list styling

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -8,18 +8,17 @@ const CartList = (props) => {
     <div className='CartList'>
       {props.customer.cart.map(item =>
         <div key={item._id} className='CartList-Container col-xs-10 col-sm-12'>
-          <div className='CartList-Image col-xs-10 col-sm-6'>
+          <div className='CartList-Image col-xs-10 col-sm-8'>
             <img className='CartList-Image' src={item.image} alt='items' />
           </div>
-          <div className='CartList-Details col-xs-7 col-sm-6'>
-            <p className='CartList-Name'>{item.name}</p>
-            <p className='CartList-Price'>${item.price}</p>
+          <div className='CartList-Details col-xs-7 col-sm-4'>
+            <p className='CartList-Name'>{item.name} - ${item.price}</p>
             <p className='CartList-Price'>Quantity: {item.quantity}</p>
-            <p className='CartList-Price'>{item.shopName}</p>
-            <p className='CartList-Price'>{item.shopCity}</p>
+            <p className='CartList-Price'>{item.shopName} - {item.shopCity}</p>
           </div>
         </div>
       )}
+        <hr/>
       <div className='col-xs-12 col-sm-12 CartList-Button'>
         {props.customer.cart.length >= 1 ? <UpdateOrderButton className='CartList-Button' /> : <h4>You have no donuts in your cart.</h4>}
       </div>
