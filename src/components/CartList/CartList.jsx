2f7208dd2094ebe3ee84eedff0dcbd1c1b464f45
Julia Yi <juliamijiyi@gmail.com>
2017-06-08 14:51:11 -0700

    Style cart view

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -7,14 +7,19 @@ const CartList = (props) => {
   return (
     <div className='CartList'>
       {props.customer.cart.map(item =>
-        <div key={item._id} className='CartList-Image-Container col-xs-10 col-sm-4'>
-          <img className='CartList-Image' src={item.image} alt='items' />
-          <p className='CartList-Caption CartList-Name'>{item.name}</p>
-          <p className='CartList-Caption CartList-Price'>${item.price}</p>
-          <p className='CartList-Caption CartList-Price'>Quantity: {item.quantity}</p>
-          <p className='CartList-Caption CartList-Price'>{item.shopName}</p>
-          <p className='CartList-Caption CartList-Price'>{item.shopCity}</p>
+        <div key={item._id} className='CartList-Container col-xs-10 col-sm-12'>
+          <div className='CartList-Image col-xs-10 col-sm-6'>
+            <img className='CartList-Image' src={item.image} alt='items' />
+          </div>
+          <div className='CartList-Details col-xs-7 col-sm-6'>
+            <p className='CartList-Name'>{item.name}</p>
+            <p className='CartList-Price'>${item.price}</p>
+            <p className='CartList-Price'>Quantity: {item.quantity}</p>
+            <p className='CartList-Price'>{item.shopName}</p>
+            <p className='CartList-Price'>{item.shopCity}</p>
+          </div>
         </div>
+        <hr/>
       )}
       <div className='col-xs-12 col-sm-12 CartList-Button'>
         {props.customer.cart.length >= 1 ? <UpdateOrderButton className='CartList-Button' /> : <h4>You have no donuts in your cart.</h4>}
